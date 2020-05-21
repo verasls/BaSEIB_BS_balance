58b7c82c4a0c1c1e04b0b9d5b9d7465845b1f793
Lucas Veras <lucasdsveras@gmail.com>
2020-05-21 16:10:57 +0100

    Document function

--- code/functions/map_levene.R
+++ code/functions/map_levene.R
@@ -1,6 +1,18 @@
 map_levene <- function(data, vars, group) {
+  # Map the car::leveneTest() function using a list of variables and a list of 
+  # data frames
+  #
+  # Args:
+  #   data: A list of data frames (or tibbles)
+  #   vars: A list of character strings
+  #   group: A character string with the name of the grouping variable
+  #
+  # Returns:
+  #   A list of data frames (or tibbles) with the F ratio and
+  #   p value
+
   tests <- purrr::map(vars, ~ data[[.x]]) %>% 
-    purrr::map(~ leveneTest(.x, data[[group]]))
+    purrr::map(~ car::leveneTest(.x, data[[group]]))
   tibble::tibble(variables = vars) %>% 
     dplyr::mutate(
       F_value = purrr::map(tests, "F value") %>% purrr::map_dbl(~ .x[[1]]),
