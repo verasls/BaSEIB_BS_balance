9819a33111b8e854d68aea0388accd06e54c897a
Lucas Veras <lucasdsveras@gmail.com>
2020-05-21 11:40:42 +0100

    Add leveneTest()

--- code/02_explore.R
+++ code/02_explore.R
@@ -4,6 +4,7 @@ library(here)
 library(tidyverse)
 library(car)
 source(here("code", "functions", "map_shapiro.R"))
+source(here("code", "functions", "map_levene.R"))
 
 # Load and prepare data -------------------------------------------------------
 
@@ -18,17 +19,20 @@ int_0m <- df_long %>%
 int_6m <- df_long %>%
   filter(group == "Intervention", time == "6 months after")
 
+df_control <- df_long %>% filter(group == "Control")
+df_intervention <- df_long %>% filter(group == "Intervention")
+df_0m <- df_long %>% filter(time == "Baseline")
+df_6m <- df_long %>% filter(time == "6 months after")
+
 # Make a list of data frames to be used in the map functions
 data <- list(con_0m, con_6m, int_0m, int_6m)
 
-# Normality tests -------------------------------------------------------------
-
+# Separate the variables by "group"
 # Demographic and anthropometric variables
 demo_anthro_vars <- c(
   "age", "height", "weight", "bmi", 
   "waist_circumference", "hip_circumference", "waist_hip_ratio"
 )
-map(data, ~ map_shapiro(.x, demo_anthro_vars))
 
 # Body composition variables
 body_comp_vars <- c(
@@ -36,7 +40,6 @@ body_comp_vars <- c(
   "android_gynoid_ratio", "lower_limbs_total_mass", "upper_body_total_mass",
   "whole_body_total_mass", "whole_body_fat_mass", "whole_body_lean_mass"
 )
-map(data, ~ map_shapiro(.x, body_comp_vars))
 
 # Strength variables
 strength_vars <- c(
@@ -44,18 +47,15 @@ strength_vars <- c(
   "pt_knee_60ds_exten_divided_whole_body_total_mass",
   "pt_knee_60ds_flexi_divided_whole_body_total_mass"
 )
-map(data, ~ map_shapiro(.x, strength_vars))
 
 # Balance variables
 balance_vars <- c(
   "ellipse_eo", "vap_eo", "sdap_eo", "vml_eo", "sdml_eo", "vt_eo",
   "ellipse_ec", "vap_ec", "sdap_ec", "vml_ec", "sdml_ec", "vt_ec"
 )
-map(data, ~ map_shapiro(.x, balance_vars))
 
 # Physical activity variables
 pa_vars <- c("steps", "sb", "lpa", "mpa", "vpa", "mvpa")
-map(data, ~ map_shapiro(.x, pa_vars))
 
 # SF36 variables
 sf36_vars <- c(
@@ -64,4 +64,44 @@ sf36_vars <- c(
   "role_limitations_emotional", "mental_health", 
   "physical_component", "mental_component"
 )
+
+# Normality tests -------------------------------------------------------------
+
+map(data, ~ map_shapiro(.x, demo_anthro_vars))
+map(data, ~ map_shapiro(.x, body_comp_vars))
+map(data, ~ map_shapiro(.x, strength_vars))
+map(data, ~ map_shapiro(.x, balance_vars))
+map(data, ~ map_shapiro(.x, pa_vars))
 map(data, ~ map_shapiro(.x, sf36_vars))
+
+# Test for homocedasticity ----------------------------------------------------
+
+# By time
+map_levene(df_control, demo_anthro_vars, "time")
+map_levene(df_control, body_comp_vars, "time")
+map_levene(df_control, strength_vars, "time")
+map_levene(df_control, balance_vars, "time")
+map_levene(df_control, pa_vars, "time")
+map_levene(df_control, sf36_vars, "time")
+
+map_levene(df_intervention, demo_anthro_vars, "time")
+map_levene(df_intervention, body_comp_vars, "time")
+map_levene(df_intervention, strength_vars, "time")
+map_levene(df_intervention, balance_vars, "time")
+map_levene(df_intervention, pa_vars, "time")
+map_levene(df_intervention, sf36_vars, "time")
+
+# By group
+map_levene(df_0m, demo_anthro_vars, "group")
+map_levene(df_0m, body_comp_vars, "group")
+map_levene(df_0m, strength_vars, "group")
+map_levene(df_0m, balance_vars, "group")
+map_levene(df_0m, pa_vars, "group")
+map_levene(df_0m, sf36_vars, "group")
+
+map_levene(df_6m, demo_anthro_vars, "group")
+map_levene(df_6m, body_comp_vars, "group")
+map_levene(df_6m, strength_vars, "group")
+map_levene(df_6m, balance_vars, "group")
+map_levene(df_6m, pa_vars, "group")
+map_levene(df_6m, sf36_vars, "group")
